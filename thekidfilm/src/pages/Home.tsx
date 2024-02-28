4684405eea5ef44699feb0e317aced6f572df0a7
diegobaena07 <145508134+diegobaena07@users.noreply.github.com>
2024-02-27 23:50:46 -0500

    Ajustes del Proyecto Inicio

---
+++ thekidfilm/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home_background">
+      <div className='Home_Inicio'>
+        <img src="THE KID FLIM LOGO letras blancas sin fondo logo en color_4.webp" alt="TheKidFilm" />
+        <button className='Btn_PrincipalFilm'>CONOCENOS</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
