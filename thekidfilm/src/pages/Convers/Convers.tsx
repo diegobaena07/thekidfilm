02a72b604311ffa6970c5334a352f1f1a2f329d3
diegobaena07 <145508134+diegobaena07@users.noreply.github.com>
2024-03-02 22:49:10 -0500

    Pantalla de covers lista

--- thekidfilm/src/pages/Convers/Convers.tsx
+++ thekidfilm/src/pages/Convers/Convers.tsx
@@ -1,15 +1,87 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./Convers.css";
 import { Header } from "../../components/header/Header";
 import { Footer } from "../../components/footer/Footer";
 
 const Convers: FC = () => {
 
+    const [showDescription, setShowDescription] = useState(false);
+
+    const toggleDescription = () => {
+        setShowDescription(!showDescription);
+    };
+
     return (
         <>
             <Header />
             <div className="container">
-                
+                <div className="Covers_ImagenesContainer">
+                    <div className="Covers_PortadaContainer" onClick={toggleDescription}>
+                        <img src="Rectangle 20 (1).png" alt="coverThekidFilm" />
+                        {showDescription && (
+                            <div className="Covers_Descripcion">
+                                <h2>Title primary</h2>
+                                <p>
+                                    Rorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Etiam eu turpis molestie, dictum est a, mattis tellus.
+                                    Sed dignissim, metus nec fringilla accumsan.
+                                </p>
+                            </div>
+                        )}
+                    </div>
+                    <div className="Covers_PortadaContainer">
+                        <img src="Rectangle 20 (1).png" alt="coverThekidFilm" />
+                        <div className="Covers_Descripcion">
+                            <h2>Title primary</h2>
+                            <p>Rorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Etiam eu turpis molestie, dictum est a, mattis tellus.
+                                Sed dignissim, metus nec fringilla accumsan.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="Covers_PortadaContainer">
+                        <img src="Rectangle 20 (1).png" alt="coverThekidFilm" />
+                        <div className="Covers_Descripcion">
+                            <h2>Title primary</h2>
+                            <p>Rorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Etiam eu turpis molestie, dictum est a, mattis tellus.
+                                Sed dignissim, metus nec fringilla accumsan.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Covers_ImagenesContainer">
+                    <div className="Covers_PortadaContainer">
+                        <img src="Rectangle 20 (1).png" alt="coverThekidFilm" />
+                        <div className="Covers_Descripcion">
+                            <h2>Title primary</h2>
+                            <p>Rorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Etiam eu turpis molestie, dictum est a, mattis tellus.
+                                Sed dignissim, metus nec fringilla accumsan.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="Covers_PortadaContainer">
+                        <img src="Rectangle 20 (1).png" alt="coverThekidFilm" />
+                        <div className="Covers_Descripcion">
+                            <h2>Title primary</h2>
+                            <p>Rorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Etiam eu turpis molestie, dictum est a, mattis tellus.
+                                Sed dignissim, metus nec fringilla accumsan.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="Covers_PortadaContainer">
+                        <img src="Rectangle 20 (1).png" alt="coverThekidFilm" />
+                        <div className="Covers_Descripcion">
+                            <h2>Title primary</h2>
+                            <p>Rorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Etiam eu turpis molestie, dictum est a, mattis tellus.
+                                Sed dignissim, metus nec fringilla accumsan.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
